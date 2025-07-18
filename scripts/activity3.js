787eb6dbc31f64268185e4cec789a8792525646c
Glavin <110996095+Glanvin@users.noreply.github.com>
2025-07-18 21:35:36 +0800

    activity 3 for compsci 4

---
+++ scripts/activity3.js
@@ -0,0 +1,8 @@
+let firstName = 'Seth Matthew';
+let lastName = 'Valmores';
+let birthYear = 2004;
+let currentYear = 2025;
+let age = currentYear - birthYear;
+let address = 'Yacapin Street. CDO'
+document.writeln('Hello! My name is ' + firstName + ' ' + lastName + ' I live in ' + address + '\n' + 'I\'m ' + age + ' years old')
+alert('Hello! My name is ' + firstName + ' ' + lastName + ' I live in ' + address + '\n' + 'I\'m ' + age + ' years old')
