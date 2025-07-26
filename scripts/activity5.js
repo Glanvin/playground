1f5109c9ddda5d8d38de1b966c8a50fc84395026
Glavin <110996095+Glanvin@users.noreply.github.com>
2025-07-26 13:29:02 +0800

    activity 5 for compsci 4

--- scripts/activity5.js
+++ scripts/activity5.js
@@ -1,9 +1,31 @@
-let firstName = prompt('Enter your first name');
+let firstName = prompt('Enter your first name');//Gets the first name of the user
 let lastName = prompt('Enter your last name');//Gets the last name of the user
 let address = prompt('Enter your current address');//Cuurent address of the user
-let birthYear = prompt('Enter your birth year');//Birthyear of the user
+let birthYear;
+
+
+/*
+ *A while loop to if the birthYear value is not a number
+ *It will only exit if the inputted value is a number and that is a valid year between 1900 to the current year
+ * **/
+let isValidBirthYear = false;
+
+while (!isValidBirthYear) {
+  birthYear = prompt("Enter your birth year (e.g 1980)")
+  if (isNaN(birthYear)) {
+    alert("Invalid number. Please in put a valid number!");
+  } else {
+    if (birthYear < 1900 || birthYear > new Date().getFullYear()) {
+      alert("Enter a valid birthYear!")
+    } else {
+      isValidBirthYear = true
+    }
+  }
+}
+
 const currentYear = new Date(); //Gets the current month, day and year
 let curentAge = currentYear.getFullYear() - birthYear; //Gets current year then subtracts the birth year
+
 let isAdult = (age) => { // A lamda function if you want to have a short function that is only 1 - 3 lines or simple operations
   return age >= 18 ? 'Adult' : 'Minor'//This an ternary operator basically a shorten if else statement
 }
