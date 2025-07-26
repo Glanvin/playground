8d00f09ad9f5823c9c3c312a1c8dd4aa6a9204bf
Glavin <110996095+Glanvin@users.noreply.github.com>
2025-07-26 12:36:38 +0800

    activity 3 for compsci 4

--- scripts/activity3.js
+++ scripts/activity3.js
@@ -1,8 +1,16 @@
+/*
+ *
+ * Variables to store values of my infomation
+ * */
 let firstName = 'Seth Matthew';
 let lastName = 'Valmores';
 let birthYear = 2004;
 let currentYear = 2025;
 let age = currentYear - birthYear;
-let address = 'Yacapin Street. CDO'
-document.writeln('Hello! My name is ' + firstName + ' ' + lastName + ' I live in ' + address + '\n' + 'I\'m ' + age + ' years old')
-alert('Hello! My name is ' + firstName + ' ' + lastName + ' I live in ' + address + '\n' + 'I\'m ' + age + ' years old')
+let address = 'Yacapin Street. CDO';
+let greet = 'Hello! My name is ' + firstName + ' ' + lastName + ' I live in ' + address + ' I\'m ' + age + ' years old';
+/*
+ *document.writeln() and alert() is write my stored values into the personalInfo.html
+ * */
+document.writeln(greet)
+alert(greet)
